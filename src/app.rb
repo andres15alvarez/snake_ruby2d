0b15619d219033d38a154d35b5f8bd911ccad05b
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 18:06:04 -0400

    Set initial state in app

--- src/app.rb
+++ src/app.rb
@@ -1,1 +1,7 @@
-puts "Hello Ruby"
+require_relative "views/ruby2d"
+require_relative "models/state"
+require_relative "utils/initial_state"
+
+view = View::Ruby2dView.new
+state = initial_state()
+view.render(state)
