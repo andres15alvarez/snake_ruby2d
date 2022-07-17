617657f3ea42ead3fb8007c54cb57e5f1afce988
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 19:07:50 -0400

    Update to use only class and create methods

--- src/views/ruby2d.rb
+++ src/views/ruby2d.rb
@@ -1,16 +1,45 @@
 require "ruby2d"
 
-module View
-  class Ruby2dView
+class Ruby2dView
 
-    def initialize
-    end
-
-    def render(state)
-      extend Ruby2D::DSL
-      set(title: "Snake", width: 900, height: 600)
-      show
-    end
-
+  def initialize
+    @pixel_size = 50
   end
+
+  def render(state)
+    extend Ruby2D::DSL
+    set(
+      title: "Snake",
+      width: @pixel_size * state.grid.cols,
+      height: @pixel_size * state.grid.rows
+    )
+    render_snake(state)
+    render_food(state)
+    show
+  end
+
+  private
+
+  def render_food(state)
+    extend Ruby2D::DSL
+    Square.new(
+      x: state.food.col * @pixel_size,
+      y: state.food.row * @pixel_size,
+      size: @pixel_size,
+      color: 'orange'
+    )
+  end
+
+  def render_snake(state)
+    extend Ruby2D::DSL
+    state.snake.positions.each do |pos|
+      Square.new(
+        x: pos.col * @pixel_size,
+        y: pos.row * @pixel_size,
+        size: @pixel_size,
+        color: 'green'
+      )
+    end
+  end
+
 end
