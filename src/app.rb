689ef7b84f9f643672d6b6ce8ba64fe6a60e4ca0
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 19:08:03 -0400

    Update usage of view

--- src/app.rb
+++ src/app.rb
@@ -2,6 +2,6 @@ require_relative "views/ruby2d"
 require_relative "models/state"
 require_relative "utils/initial_state"
 
-view = View::Ruby2dView.new
+view = Ruby2dView.new
 state = initial_state()
 view.render(state)
