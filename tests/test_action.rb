c7fcb135cb449d3e0ec2aae9189d547981768e90
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 23:18:52 -0400

    Add tests for change of direction

--- tests/test_action.rb
+++ tests/test_action.rb
@@ -9,27 +9,51 @@ require_relative "../src/utils/direction"
 require_relative "../src/actions/actions"
 
 class ActionTest < Minitest::Test
+  def setup
+    @state = initial_state()
+  end
+
   def test_move_snake
-    state = initial_state()
     expected_state = State.new(
       Snake.new([Coordinate.new(2, 1), Coordinate.new(1, 1)]),
       Food.new(4, 4),
       Grid.new(12, 15),
       Direction::DOWN
     )
-    actual_state = move_snake(state)
+    actual_state = move_snake(@state)
     assert_equal(expected_state, actual_state)
   end
 
   def test_move_snake_to_position
-    state = initial_state()
     expected_state = State.new(
       Snake.new([Coordinate.new(2, 1), Coordinate.new(1, 1)]),
       Food.new(4, 4),
       Grid.new(12, 15),
       Direction::DOWN
     )
-    actual_state = move_snake_to_position(state, Coordinate.new(2, 1))
+    actual_state = move_snake_to_position(@state, Coordinate.new(2, 1))
+    assert_equal(expected_state, actual_state)
+  end
+
+  def test_change_direction
+    expected_state = State.new(
+      Snake.new([Coordinate.new(1, 1), Coordinate.new(0, 1)]),
+      Food.new(4, 4),
+      Grid.new(12, 15),
+      Direction::LEFT
+    )
+    actual_state = change_direction(@state, Direction::LEFT)
+    assert_equal(expected_state, actual_state)
+  end
+
+  def test_change_direction_invalid
+    expected_state = State.new(
+      Snake.new([Coordinate.new(1, 1), Coordinate.new(0, 1)]),
+      Food.new(4, 4),
+      Grid.new(12, 15),
+      Direction::DOWN
+    )
+    actual_state = change_direction(@state, Direction::UP)
     assert_equal(expected_state, actual_state)
   end
 end
