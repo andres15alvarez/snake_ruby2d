5ff4b36227d384cd9173b8a48421719cdd783eea
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 23:18:30 -0400

    Use class and run it

--- src/app.rb
+++ src/app.rb
@@ -1,7 +1,39 @@
 require_relative "views/ruby2d"
 require_relative "models/state"
 require_relative "utils/initial_state"
+require_relative "actions/actions"
 
-view = Ruby2dView.new
-state = initial_state()
-view.render(state)
+class App
+  def initialize
+    @state = initial_state()
+  end
+
+  def start
+    @view = Ruby2dView.new(self)
+    Thread.new { init_timer(@view) }
+    @view.start(@state)
+    @view.render_game(@state)
+  end
+
+  def init_timer(view)
+    loop do
+      if @state.is_game_finished
+        break
+      end
+      @state = move_snake(@state)
+      view.render_game(@state)
+      sleep 0.5
+    end
+  end
+
+  def send_action(action, params)
+    new_state = method(action).call(@state, params)
+    if new_state.hash != @state.hash
+      @state = new_state
+      @view.render(@state)
+    end
+  end
+end
+
+app = App.new()
+app.start()
