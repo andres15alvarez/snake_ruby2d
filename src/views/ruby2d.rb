e1e7194afe9238311166d07df6d9c068a353635e
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 23:17:57 -0400

    Add logic to render new actions

--- src/views/ruby2d.rb
+++ src/views/ruby2d.rb
@@ -1,28 +1,40 @@
 require "ruby2d"
+require_relative "../actions/actions"
+require_relative "../utils/handle_input"
 
 class Ruby2dView
 
-  def initialize
+  def initialize(app)
     @pixel_size = 50
+    @app = app
   end
 
-  def render(state)
+  def start(state)
     extend Ruby2D::DSL
     set(
       title: "Snake",
       width: @pixel_size * state.grid.cols,
       height: @pixel_size * state.grid.rows
     )
+    on :key_down do |event|
+      @app.send_action(:change_direction, handle_input(event.key))
+    end
+    show
+  end
+
+  def render_game(state)
+    extend Ruby2D::DSL
+    close if state.is_game_finished
     render_snake(state)
     render_food(state)
-    show
   end
 
   private
 
   def render_food(state)
+    @food.remove() if @food
     extend Ruby2D::DSL
-    Square.new(
+    @food = Square.new(
       x: state.food.col * @pixel_size,
       y: state.food.row * @pixel_size,
       size: @pixel_size,
@@ -31,8 +43,13 @@ class Ruby2dView
   end
 
   def render_snake(state)
+    if @snake_positions
+      @snake_positions.each do |pos|
+        pos.remove()
+      end
+    end
     extend Ruby2D::DSL
-    state.snake.positions.each do |pos|
+    @snake_positions = state.snake.positions.map do |pos|
       Square.new(
         x: pos.col * @pixel_size,
         y: pos.row * @pixel_size,
@@ -41,5 +58,4 @@ class Ruby2dView
       )
     end
   end
-
 end
