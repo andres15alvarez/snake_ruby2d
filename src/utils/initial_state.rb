2e229bfd92e7d354d77d7aa8dd19d09501edf543
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 20:53:02 -0400

    Update initial state

--- src/utils/initial_state.rb
+++ src/utils/initial_state.rb
@@ -3,11 +3,13 @@ require_relative "../models/coordinate"
 require_relative "../models/snake"
 require_relative "../models/food"
 require_relative "../models/grid"
+require_relative "direction"
 
 def initial_state
   State.new(
-    Snake.new([Coordinate.new(1, 1), Coordinate.new(0,1)]),
+    Snake.new([Coordinate.new(1, 1), Coordinate.new(0, 1)]),
     Food.new(4, 4),
-    Grid.new(12, 15)
+    Grid.new(12, 15),
+    Direction::DOWN
   )
 end
