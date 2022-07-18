6a71389c2281d4159c6ca1df9380627fc6d96a52
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-17 20:52:40 -0400

    Add more properties

--- src/models/state.rb
+++ src/models/state.rb
@@ -1,8 +1,19 @@
 class State
-  attr_accessor :snake, :food, :grid
-  def initialize(snake, food, grid)
+  attr_accessor :snake, :food, :grid, :next_direction, :is_game_finished
+  def initialize(snake, food, grid, next_direction)
     @snake = snake
     @food = food
     @grid = grid
+    @next_direction = next_direction
+    @is_game_finished = false
+  end
+
+  def ==(other)
+    (
+      @snake == other.snake and
+      @food == other.food and
+      @grid == other.grid and
+      @is_game_finished == other.is_game_finished
+    )
   end
 end
