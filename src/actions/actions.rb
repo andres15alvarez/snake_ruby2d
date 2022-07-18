8cc4a1b2a8a6f9dca23263425da502de5f5e2fe4
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-18 18:47:41 -0400

    Add documentation for functions

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -2,19 +2,23 @@ require_relative '../utils/direction'
 require_relative '../models/coordinate'
 require_relative '../models/food'
 
+# Move the snake updating the state of the game
 # @param state [State]
+# @return [State]
 def move_snake(state)
   next_position = calculate_next_position(state)
-  if is_position_food?(state, next_position)
+  if position_food?(state, next_position)
     state = grow_snake_to(state, next_position)
     generate_food(state)
-  elsif is_position_valid?(state, next_position)
+  elsif position_valid?(state, next_position)
     move_snake_to_position(state, next_position)
   else
     end_game(state)
   end
 end
 
+# @param state [State]
+# @return [State]
 def calculate_next_position(state)
   new_position = state.snake.positions.first
   case state.next_direction
@@ -29,7 +33,10 @@ def calculate_next_position(state)
   end
 end
 
-def is_position_valid?(state, position)
+# @param state [State]
+# @param position [Coordinate]
+# @return [Boolean]
+def position_valid?(state, position)
   is_inside_grid = (
     (position.row < state.grid.rows and position.row >= 0) and
     (position.col < state.grid.cols and position.col >= 0)
@@ -38,13 +45,19 @@ def is_position_valid?(state, position)
   is_inside_grid and !is_snake_position
 end
 
+# @param state [State]
+# @param position [Coordinate]
+# @return [State]
 def move_snake_to_position(state, position)
   new_positions = [position] + state.snake.positions[0...-1]
   state.snake.positions = new_positions
   state
 end
 
-def is_next_direction_valid?(state, direction)
+# @param state [State]
+# @param direction [Direction]
+# @return [Boolean]
+def next_direction_valid?(state, direction)
   case state.next_direction
   when Direction::UP
     return true if direction != Direction::DOWN
@@ -57,28 +70,39 @@ def is_next_direction_valid?(state, direction)
   end
 end
 
+# @param state [State]
+# @param direction [Direction]
+# @return [State]
 def change_direction(state, direction)
-  if is_next_direction_valid?(state, direction)
-    state.next_direction = direction
-  end
+  state.next_direction = direction if next_direction_valid?(state, direction)
   state
 end
 
+# @param state [State]
+# @param next_position [Coordinate]
+# @return [State]
 def grow_snake_to(state, next_position)
   state.snake.positions = [next_position] + state.snake.positions
   state
 end
 
-def is_position_food?(state, position)
+# @param state [State]
+# @param position [Coordinate]
+# @return [Boolean]
+def position_food?(state, position)
   state.food == position
 end
 
+# @param state [State]
+# @return [State]
 def generate_food(state)
   new_food = Food.new(rand(0...state.grid.rows), rand(0...state.grid.cols))
   state.food = new_food
   state
 end
 
+# @param state [State]
+# @return [State]
 def end_game(state)
   state.is_game_finished = true
   state
